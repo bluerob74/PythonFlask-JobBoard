10e7a837204967836b07e9d4524c201275939a40
bluerob74 <rob.bluemke@gmail.com>
2018-11-13 15:44:44 -0600

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -5,7 +5,7 @@ PATH ='db/jobs.sqlite'
 
 app = Flask(__name__)
 
-def open_connection()
+def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
@@ -24,7 +24,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     return results
 
 @app.teardown_appcontext
-def close_connection(exception)
+def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
